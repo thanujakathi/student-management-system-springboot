00f7185648d0200a36236094dbf92e56828c668d
thanujakathi <thanujakathi815@gmail.com>
2025-02-25 21:01:17 +0530

    student management changes

---
+++ src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,30 @@
+package net.javaguides.sms;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		/*
+		 * Student student1 = new Student("Ramesh", "Fadatare", "ramesh@gmail.com");
+		 * studentRepository.save(student1);
+		 * 
+		 * Student student2 = new Student("Sanjay", "Jadhav", "sanjay@gmail.com");
+		 * studentRepository.save(student2);
+		 * 
+		 * Student student3 = new Student("tony", "stark", "tony@gmail.com");
+		 * studentRepository.save(student3);
+		 */
+		
+	}
+
+}
